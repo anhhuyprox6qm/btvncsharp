5cbe11abe0cb497122456141ea1b04d3dbbf48a7
Huy <73454962+anhhuyprox6qm@users.noreply.github.com>
2021-06-16 17:59:51 +0700

    Update Person.cs

--- Lab5/Exercise 2/Person.cs
+++ Lab5/Exercise 2/Person.cs
@@ -2,13 +2,11 @@
 {
     public abstract class Person
     {
-        public string IdentityNumber { get; set; }
+        public string IdentityNumer { get; set; }
         public string FullName { get; set; }
-        public string BaseSalary { get; set; }
-        public string PersonName { get; set; }
-        public int SkillLevel { get; set; }
-        public double SeniorLecture { get; set; }
+        public double BaseSalary { get; set; }
+        public int Skilllevel { get; set; } 
+        public double SeniorLecture { get; set; } 
         public double Bonus { get; set; }
-        public abstract double CalculateSalary();
     }
 }
